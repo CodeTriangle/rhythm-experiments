70411942b04b1b81f67d744fab0161529bc5c5b9
Reuben Staley <lighthousemaniac@gmail.com>
2022-08-27 00:23:31 -0600

    %retab!

--- src/main.c
+++ src/main.c
@@ -6,13 +6,13 @@
 // modification, are permitted provided that the following conditions are
 // met:
 // 
-//     (1) Redistributions of source code must retain the above copyright
-//     notice, this list of conditions and the following disclaimer. 
+//	   (1) Redistributions of source code must retain the above copyright
+//	   notice, this list of conditions and the following disclaimer. 
 // 
-//     (2) Redistributions in binary form must reproduce the above
-//     copyright notice, this list of conditions and the following
-//     disclaimer in the documentation and/or other materials provided with
-//     the distribution.  
+//	   (2) Redistributions in binary form must reproduce the above
+//	   copyright notice, this list of conditions and the following
+//	   disclaimer in the documentation and/or other materials provided with
+//	   the distribution.  
 // 
 // THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
 // IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -36,306 +36,306 @@
 #include "../include/constants.h"
 
 typedef struct {
-    unsigned int pressed: 1;
-    signed int value: 15;
+	unsigned int pressed: 1;
+	signed int value: 15;
 } Beat;
 
 int main(int argc, char **argv) {
-    float bpm = BPM;
-    int beatProximity = BEAT_PROX;
-    int soundAnticipation = ANTICIPATION;
-    int drawing = 0;
-    int playMusic = 0;
-    char *musPath = "assets/music.mp3";
-    int musDelay = 0;
-    int playMet = 0;
-    char *metPath = "assets/metronome.wav";
-    char *pattern = "1";
-    int patternLength = 1;
+	float bpm = BPM;
+	int beatProximity = BEAT_PROX;
+	int soundAnticipation = ANTICIPATION;
+	int drawing = 0;
+	int playMusic = 0;
+	char *musPath = "assets/music.mp3";
+	int musDelay = 0;
+	int playMet = 0;
+	char *metPath = "assets/metronome.wav";
+	char *pattern = "1";
+	int patternLength = 1;
 
-    int i;
+	int i;
 
-    for (i = 1; i < argc; i++) {
-        if (argv[i][0] == '-') {
-            char *option = argv[i]+1;
-            if (strcmp(option, "bpm") == 0) {
-                bpm = atof(argv[++i]);
-            } else if (strcmp(option, "prox") == 0) {
-                beatProximity = atoi(argv[++i]);
-            } else if (strcmp(option, "draw") == 0) {
-                drawing = 1;
-            } else if (strcmp(option, "mus") == 0) {
-                playMusic = 1;
-            } else if (strcmp(option, "musp") == 0) {
-                musPath = argv[++i];
-            } else if (strcmp(option, "musd") == 0) {
-                musDelay = atoi(argv[++i]);
-            } else if (strcmp(option, "met") == 0) {
-                playMet = 1;
-            } else if (strcmp(option, "metp") == 0) {
-                metPath = argv[++i];
-            } else if (strcmp(option, "metant") == 0) {
-                soundAnticipation = atoi(argv[++i]);
-            } else if (strcmp(option, "pat") == 0) {
-                pattern = argv[++i];
-                patternLength = strlen(pattern);
-            } else {
-                printf("Unexpected argument (%d): %s\n", i, option);
-            }
-        } else {
-            printf("Unexpected bare argument (%d): %s\n", i, argv[i]);
-        }
-    }
+	for (i = 1; i < argc; i++) {
+		if (argv[i][0] == '-') {
+			char *option = argv[i]+1;
+			if (strcmp(option, "bpm") == 0) {
+				bpm = atof(argv[++i]);
+			} else if (strcmp(option, "prox") == 0) {
+				beatProximity = atoi(argv[++i]);
+			} else if (strcmp(option, "draw") == 0) {
+				drawing = 1;
+			} else if (strcmp(option, "mus") == 0) {
+				playMusic = 1;
+			} else if (strcmp(option, "musp") == 0) {
+				musPath = argv[++i];
+			} else if (strcmp(option, "musd") == 0) {
+				musDelay = atoi(argv[++i]);
+			} else if (strcmp(option, "met") == 0) {
+				playMet = 1;
+			} else if (strcmp(option, "metp") == 0) {
+				metPath = argv[++i];
+			} else if (strcmp(option, "metant") == 0) {
+				soundAnticipation = atoi(argv[++i]);
+			} else if (strcmp(option, "pat") == 0) {
+				pattern = argv[++i];
+				patternLength = strlen(pattern);
+			} else {
+				printf("Unexpected argument (%d): %s\n", i, option);
+			}
+		} else {
+			printf("Unexpected bare argument (%d): %s\n", i, argv[i]);
+		}
+	}
 
-    printf("%d\n\n", patternLength);
+	printf("%d\n\n", patternLength);
 
-    SDL_Window *window;
-    SDL_Renderer *renderer;
+	SDL_Window *window;
+	SDL_Renderer *renderer;
 
-    if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO) != 0) {
-        SDL_Log("Unable to initialize SDL2: %s\n", SDL_GetError());
-        return 1;
-    }
+	if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO) != 0) {
+		SDL_Log("Unable to initialize SDL2: %s\n", SDL_GetError());
+		return 1;
+	}
 
-    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024) < 0) {
-        SDL_Log("Unable to initialize SDL_mixer: %s\n", Mix_GetError());
-        return 1;
-    }
+	if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024) < 0) {
+		SDL_Log("Unable to initialize SDL_mixer: %s\n", Mix_GetError());
+		return 1;
+	}
 
-    window = SDL_CreateWindow(
-            WINDOW_TITLE,
-            SDL_WINDOWPOS_UNDEFINED,
-            SDL_WINDOWPOS_UNDEFINED,
-            WINDOW_WIDTH,
-            WINDOW_HEIGHT,
-            SDL_WINDOW_RESIZABLE
-    );
+	window = SDL_CreateWindow(
+		WINDOW_TITLE,
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		WINDOW_WIDTH,
+		WINDOW_HEIGHT,
+		SDL_WINDOW_RESIZABLE
+	);
 
-    if (window == NULL) {
-        SDL_Log("Could not create window: %s\n", SDL_GetError());
-        return 1;
-    }
+	if (window == NULL) {
+		SDL_Log("Could not create window: %s\n", SDL_GetError());
+		return 1;
+	}
 
-    renderer = SDL_CreateRenderer(
-            window,
-            -1,
-            SDL_RENDERER_ACCELERATED |
-            SDL_RENDERER_PRESENTVSYNC |
-            SDL_RENDERER_TARGETTEXTURE
-    );
+	renderer = SDL_CreateRenderer(
+		window,
+		-1,
+		SDL_RENDERER_ACCELERATED |
+		SDL_RENDERER_PRESENTVSYNC |
+		SDL_RENDERER_TARGETTEXTURE
+	);
 
-    if (renderer == NULL) {
-        SDL_Log("Could not create renderer: %s\n", SDL_GetError());
-        return 1;
-    }
+	if (renderer == NULL) {
+		SDL_Log("Could not create renderer: %s\n", SDL_GetError());
+		return 1;
+	}
 
-    Mix_Chunk *met = NULL;
-    if (playMet && !(met = Mix_LoadWAV(metPath))) {
-        SDL_Log("Failed to load sound effect: %s\n", Mix_GetError());
-    }
+	Mix_Chunk *met = NULL;
+	if (playMet && !(met = Mix_LoadWAV(metPath))) {
+		SDL_Log("Failed to load sound effect: %s\n", Mix_GetError());
+	}
 
-    Mix_Music *mus = NULL;
-    if (playMusic && !(mus = Mix_LoadMUS(musPath))) {
-        SDL_Log("Failed to load music: %s\n", Mix_GetError());
-    }
+	Mix_Music *mus = NULL;
+	if (playMusic && !(mus = Mix_LoadMUS(musPath))) {
+		SDL_Log("Failed to load music: %s\n", Mix_GetError());
+	}
 
-    SDL_SetRenderTarget(renderer, NULL);
-    
-    Mix_AllocateChannels(16);
+	SDL_SetRenderTarget(renderer, NULL);
+	
+	Mix_AllocateChannels(16);
 
-    int screenW = WINDOW_WIDTH;
-    int screenH = WINDOW_HEIGHT;
+	int screenW = WINDOW_WIDTH;
+	int screenH = WINDOW_HEIGHT;
 
-    float beatdelay = 1.0 / bpm * 60;
-    float beatdelayms = beatdelay * 1000;
+	float beatdelay = 1.0 / bpm * 60;
+	float beatdelayms = beatdelay * 1000;
 
-    uint32_t beatNum = 0;
-    int advanceBeat = 0;
-    int isBeat = 0, shouldPlayBeat = 1;
+	uint32_t beatNum = 0;
+	int advanceBeat = 0;
+	int isBeat = 0, shouldPlayBeat = 1;
 
-    uint32_t previousBeat, nextBeat;
-    uint32_t sincePrevBeat, tillNextBeat;
+	uint32_t previousBeat, nextBeat;
+	uint32_t sincePrevBeat, tillNextBeat;
 
-    uint32_t nextSound;
+	uint32_t nextSound;
 
-    uint32_t currentTime;
+	uint32_t currentTime;
 
-    int hasKeypress = 0;
-    SDL_KeyboardEvent kbEvent;
+	int hasKeypress = 0;
+	SDL_KeyboardEvent kbEvent;
 
-    Beat keypresses[HISTORY_LENGTH] = {0};
+	Beat keypresses[HISTORY_LENGTH] = {0};
 
-    uint32_t prevToKeypress, nextToKeypress;
+	uint32_t prevToKeypress, nextToKeypress;
 
-    int x, y, lastX, lastY;
+	int x, y, lastX, lastY;
 
-    int loop = 1;
+	int loop = 1;
 
-    previousBeat = SDL_GetTicks();
-    nextBeat = previousBeat + beatdelayms;
-    nextSound = nextBeat - soundAnticipation;
+	previousBeat = SDL_GetTicks();
+	nextBeat = previousBeat + beatdelayms;
+	nextSound = nextBeat - soundAnticipation;
 
-    while (loop) {
-        SDL_Event e;
+	while (loop) {
+		if (playMusic && !Mix_PlayingMusic() && currentTime >= musDelay) {
+			Mix_PlayMusic(mus, -1);
+		}
 
-        while (SDL_PollEvent(&e)) {
-            if (e.type == SDL_KEYDOWN) {
-                switch (e.key.keysym.sym) {
-                    case SDLK_ESCAPE:
-                        loop = 0;
-                        break;
-                    case SDLK_SPACE:
-                        drawing = !drawing;
-                        break;
-                    default:
-                        hasKeypress = 1;
-                        kbEvent = e.key;
-                        break;
-                }
-            }
-            else if (e.type == SDL_QUIT) {
-                loop = 0;
-            }
-        }
+		SDL_Event e;
 
-        SDL_GetWindowSize(window, &screenW, &screenH);
+		while (SDL_PollEvent(&e)) {
+			if (e.type == SDL_KEYDOWN) {
+				switch (e.key.keysym.sym) {
+				case SDLK_ESCAPE:
+					loop = 0;
+					break;
+				case SDLK_SPACE:
+					drawing = !drawing;
+					break;
+				default:
+					hasKeypress = 1;
+					kbEvent = e.key;
+					break;
+				}
+			}
+			else if (e.type == SDL_QUIT) {
+				loop = 0;
+			}
+		}
 
-        currentTime = SDL_GetTicks();
+		SDL_GetWindowSize(window, &screenW, &screenH);
 
-        sincePrevBeat = currentTime - previousBeat;
-        tillNextBeat  = nextBeat - currentTime;
-        isBeat = sincePrevBeat <= beatProximity || tillNextBeat <= beatProximity;
+		currentTime = SDL_GetTicks();
 
-        if (sincePrevBeat >= beatdelayms) {
-            previousBeat = nextBeat;
-            nextBeat = previousBeat + beatdelayms;
+		sincePrevBeat = currentTime - previousBeat;
+		tillNextBeat  = nextBeat - currentTime;
+		isBeat = sincePrevBeat <= beatProximity || tillNextBeat <= beatProximity;
 
-            if (beatNum >= HISTORY_LENGTH) {
-                advanceBeat = 0;
-            }
+		if (sincePrevBeat >= beatdelayms) {
+			previousBeat = nextBeat;
+			nextBeat = previousBeat + beatdelayms;
 
-            sincePrevBeat = currentTime - previousBeat;
-            tillNextBeat  = nextBeat - currentTime;
+			if (beatNum >= HISTORY_LENGTH) {
+				advanceBeat = 0;
+			}
 
-            if (advanceBeat) beatNum++;
-        }
+			sincePrevBeat = currentTime - previousBeat;
+			tillNextBeat  = nextBeat - currentTime;
 
-        if (advanceBeat) {
-            int patternNum = beatNum;
+			if (advanceBeat) beatNum++;
+		}
 
-            if (sincePrevBeat < tillNextBeat) {
-                patternNum -= 1;
-            }
+		if (advanceBeat) {
+			int patternNum = beatNum;
 
-            patternNum %= patternLength;
+			if (sincePrevBeat < tillNextBeat) {
+				patternNum -= 1;
+			}
 
-            if (patternNum < 0) {
-                patternNum = 0;
-            }
+			patternNum %= patternLength;
 
-            switch (pattern[patternNum]) {
-                case '1':
-                    shouldPlayBeat = 1;
-                    break;
-                case '0':
-                    shouldPlayBeat = 0;
-                    break;
-            }
-        }
+			if (patternNum < 0) {
+				patternNum = 0;
+			}
 
-        if (playMet && currentTime >= nextSound) {
-            nextSound = nextSound + beatdelayms;
-            
-            if (shouldPlayBeat) {
-                Mix_PlayChannel(-1, met, 0);
-            }
-        }
+			switch (pattern[patternNum]) {
+			case '1':
+				shouldPlayBeat = 1;
+				break;
+			case '0':
+				shouldPlayBeat = 0;
+				break;
+			}
+		}
 
-        if (playMusic && !Mix_PlayingMusic() && currentTime >= musDelay) {
-            Mix_PlayMusic(mus, -1);
-        }
+		if (playMet && currentTime >= nextSound) {
+			nextSound = nextSound + beatdelayms;
+			
+			if (shouldPlayBeat) {
+				Mix_PlayChannel(-1, met, 0);
+			}
+		}
 
-        if (hasKeypress) {
-            if (!advanceBeat && beatNum == 0) advanceBeat = 1;
+		if (hasKeypress) {
+			if (!advanceBeat && beatNum == 0) advanceBeat = 1;
 
-            prevToKeypress = DIFF(kbEvent.timestamp, previousBeat);
-            nextToKeypress = DIFF(nextBeat, kbEvent.timestamp);
+			prevToKeypress = DIFF(kbEvent.timestamp, previousBeat);
+			nextToKeypress = DIFF(nextBeat, kbEvent.timestamp);
 
-            if (prevToKeypress < nextToKeypress) {
-                keypresses[beatNum].pressed = 1;
-                keypresses[beatNum].value = prevToKeypress;
-            } else if (beatNum + 1 < HISTORY_LENGTH) {
-                int index = beatNum == 0 ? 0 : beatNum + 1;
-                keypresses[index].pressed = 1;
-                keypresses[index].value = -nextToKeypress;
-            }
+			if (prevToKeypress < nextToKeypress) {
+				keypresses[beatNum].pressed = 1;
+				keypresses[beatNum].value = prevToKeypress;
+			} else if (beatNum + 1 < HISTORY_LENGTH) {
+				int index = beatNum == 0 ? 0 : beatNum + 1;
+				keypresses[index].pressed = 1;
+				keypresses[index].value = -nextToKeypress;
+			}
 
-            printf(
-                "beat number %3d at time %5d: %5dms from last beat, %5dms from next beat\n",
-                beatNum,
-                kbEvent.timestamp,
-                prevToKeypress,
-                nextToKeypress
-            );
+			printf(
+				"beat number %3d at time %5d: %5dms from last beat, %5dms from next beat\n",
+				beatNum,
+				kbEvent.timestamp,
+				prevToKeypress,
+				nextToKeypress
+			);
 
-            hasKeypress = 0;
-        }
+			hasKeypress = 0;
+		}
 
-        int mouseX, mouseY;
-        SDL_GetMouseState(&mouseX, &mouseY);
+		int mouseX, mouseY;
+		SDL_GetMouseState(&mouseX, &mouseY);
 
-        if (isBeat && shouldPlayBeat) {
-            SDL_SetRenderDrawColor(renderer, 0, 30, 120, 255);
-        } else {
-            SDL_SetRenderDrawColor(renderer, 20, 90, 120, 255);
-        }
+		if (isBeat && shouldPlayBeat) {
+			SDL_SetRenderDrawColor(renderer, 0, 30, 120, 255);
+		} else {
+			SDL_SetRenderDrawColor(renderer, 20, 90, 120, 255);
+		}
 
-        SDL_RenderClear(renderer);
+		SDL_RenderClear(renderer);
 
-        SDL_Rect mouseRect = {
-                .x = mouseX - 8,
-                .y = mouseY - 8,
-                .w = 16,
-                .h = 16
-        };
+		SDL_Rect mouseRect = {
+			.x = mouseX - 8,
+			.y = mouseY - 8,
+			.w = 16,
+			.h = 16
+		};
 
-        SDL_SetRenderDrawColor(renderer, 250, 250, 200, 255);
+		SDL_SetRenderDrawColor(renderer, 250, 250, 200, 255);
 
-        SDL_RenderFillRect(renderer, &mouseRect);
+		SDL_RenderFillRect(renderer, &mouseRect);
 
-        SDL_RenderDrawLine(renderer, 0, screenH/2, screenW, screenH/2);
+		SDL_RenderDrawLine(renderer, 0, screenH/2, screenW, screenH/2);
 
-        for (i = 0; i < beatNum; i++) {
-            if (!drawing) {
-                break;
-            }
+		for (i = 0; i < beatNum; i++) {
+			if (!drawing) {
+				break;
+			}
 
-            if (!keypresses[i].pressed) {
-                continue;
-            }
+			if (!keypresses[i].pressed) {
+				continue;
+			}
 
-            x = screenW - screenW * (beatNum - i - 1) / (HISTORY_LENGTH - 1);
-            y = (float) screenH / 2 + keypresses[i].value / beatdelayms * screenH;
+			x = screenW - screenW * (beatNum - i - 1) / (HISTORY_LENGTH - 1);
+			y = (float) screenH / 2 + keypresses[i].value / beatdelayms * screenH;
 
-            if (i != 0) {
-                SDL_RenderDrawLine(renderer, lastX, lastY, x, y);
-            }
+			if (i != 0) {
+				SDL_RenderDrawLine(renderer, lastX, lastY, x, y);
+			}
 
-            lastX = x;
-            lastY = y;
-        }
+			lastX = x;
+			lastY = y;
+		}
 
-        SDL_RenderPresent(renderer);
-    }
+		SDL_RenderPresent(renderer);
+	}
 
-    if (met) Mix_FreeChunk(met);
-    if (mus) Mix_FreeMusic(mus);
+	if (met) Mix_FreeChunk(met);
+	if (mus) Mix_FreeMusic(mus);
 
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
 
-    Mix_Quit();
-    SDL_Quit();
+	Mix_Quit();
+	SDL_Quit();
 
-    return 0;
+	return 0;
 }
